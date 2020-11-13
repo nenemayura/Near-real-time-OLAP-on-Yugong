2646156c568bdba14455c99c00656f6e06e06b61
nenemayura <nenemayura@gmail.com>
2020-11-12 18:44:16 -0800

    Added dummy data replication code

---
+++ mockMaxCompute/src/com/constants/DatabaseConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.constants;
+
+/**
+ * @author nenem
+ *
+ */
+public class DatabaseConstants {
+
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "root";
+	
+}
---
+++ mockMaxCompute/src/replication/constants/DatabaseConstants.java
@@ -0,0 +1,13 @@
+package replication.constants;
+
+
+/**
+ * @author nenem
+ *
+ */
+public class DatabaseConstants {
+
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "root";
+	
+}
