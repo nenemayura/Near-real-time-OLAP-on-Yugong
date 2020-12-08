1d796bdd9cb5977cc32c4c99e5918d670781a836
Roopana Vuppalapati Chenchu <Roopana@users.noreply.github.com>
2020-12-07 19:43:43 -0600

    Update DBMessage.java

--- mockMaxCompute/src/com/communication/DBMessage.java
+++ mockMaxCompute/src/com/communication/DBMessage.java
@@ -1,25 +1,35 @@
-package com.communication;
-
 
 public class DBMessage {
 	
 	private RequestType reqType;
 	private String recordId;
 	private String record;
+	private String senderId= "";
+	private String receiverId= "";
+	private String messageKey = "";
 	
-
-	public DBMessage() {}
-	
-	
-	
-	public DBMessage(RequestType read, String recordID, String record) {
-		this.reqType = read;
-		this.recordId = recordID;
+	//TODO do we need time stamp here?
+	public DBMessage(RequestType reqType, String recordId, String record) {
+		this.recordId = recordId;
 		this.record = record;
+		this.reqType = reqType;
+		//TODO add query as per TTPC_H
 	}
-
-
-
+	public DBMessage() {
+	
+	}
+	public String getSenderId() {
+		return senderId;
+	}
+	public void setSenderId(String senderId) {
+		this.senderId = senderId;
+	}
+	public String getReceiverId() {
+		return receiverId;
+	}
+	public void setReceiverId(String receiverId) {
+		this.receiverId = receiverId;
+	}
 	public RequestType getReqType() {
 		return reqType;
 	}
@@ -37,5 +47,13 @@ public class DBMessage {
 	}
 	public void setRecord(String record) {
 		this.record = record;
+	}
+	public String getMessageKey() {
+		return this.reqType+"_"+ this.recordId+"_";
+		
+	}
+	public void setMessageKey(String messageKey) {
+		this.messageKey = this.reqType+"_"+ this.recordId+"_";
+		
 	}
 }
